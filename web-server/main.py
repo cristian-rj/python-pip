1c6ef3c13da89a98f5bae4373d5dbd1f07ed763d
Cristian Terminal <cristian.riverajuzga@gmail.com>
2023-10-18 21:37:10 -0500

    server

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,27 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+# Se crea la instacia
+app = FastAPI()
+
+# Se crea el recurso 
+@app.get('/') # Decorador
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse) # Decorador
+def get_list():
+    return """
+     <html>
+        <head>
+            <title>Platzi python pip</title>
+        </head>
+        <body>
+            <h1>Ambientes virtuales</h1>
+        </body>
+    </html>    
+    """
 
 def run():
     store.get_categories()
